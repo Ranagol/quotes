91d7d21425c535c56c5108c8c9737155852fddac
Andor <andorhorvat@gmail.com>
2020-03-01 17:25:33 +0100

    143

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="container">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote><!--Here we are listening to our event  @quoteAdded-->
         <app-quote-grid :quotes="quotes"></app-quote-grid><!--WTF IS THIS :quotes="quotes"  -->
-    
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
     export default {
         data(){
             return {
@@ -18,6 +19,14 @@
         },
         components: {
             appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+        },
+        methods: {
+
+            newQuote(quote){//since we added our whole qoute to the emitted event (this.$emit('quoteAdded', this.quote)) on the NewQuote.vue, now here on App.vue we have access to this this.quote..., and use it as an argument...
+
+                this.quotes.push(quote);//and we are adding this received quote to our quotes array
+            }
         }
     }
 </script>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="form-group">
+        <label for="textarea">Quote</label>
+        <textarea id="textarea" class="form-control" rows="3" v-model="quote"></textarea>
+      </div>
+      <div class="form-group">
+        <button class="btn btn-primary" @click.prevent="createNew">Add quote</button><!--prevent prevents the default behaviour of the button, because we don't want to send, submit with this button, we only want to execute our createNew method  -->
+      </div>
+    </form>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      quote: '',//this is two way binded to the textarea
+    }
+  },
+  methods:{
+    createNew(){
+      this.$emit('quoteAdded', this.quote);//here we are sending the newly created quote... Here we are emitting.
+      this.quote = '';//and after that, we are resetting it to be empty again
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
